835c0177b68ffb0a3840e5595c8b2fdcf8e0f0ce
mxm321 <15863722017@163.com>
2020-01-02 09:25:04 +0800

    引入富文本编辑器

--- src/module-hmmm/index.js
+++ src/module-hmmm/index.js
@@ -9,6 +9,13 @@
 // vue-router
 import { asyncRouterMap } from '@/router'
 import routerMaps from './router'
+import { quillEditor } from 'vue-quill-editor'
+import Vue from 'vue'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+Vue.component('quill-editor', quillEditor)
 
 // vuex
 // import app from './store/app'
--- src/module-hmmm/pages/questions-new.vue
+++ src/module-hmmm/pages/questions-new.vue
@@ -64,12 +64,6 @@
 </template>
 
 <script>
-import { quillEditor } from 'vue-quill-editor'
-import Vue from 'vue'
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
-Vue.component('quill-editor',quillEditor)
 export default {
   
   name: 'QuestionsNew',
