cca677bbbf747230ceb8d7230562649db00322ff
mxm321 <15863722017@163.com>
2020-01-05 11:09:51 +0800

    试题录入完成

--- src/module-hmmm/pages/questions-new.vue
+++ src/module-hmmm/pages/questions-new.vue
@@ -17,7 +17,7 @@
             <el-option v-for="item in company.items" :key="item.id" :value="item.id" :label="item.shortName"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="城市" prop="province" >
+        <el-form-item label="城市" prop="city" >
           <el-select 
           style="width:180px" 
           v-model="addForm.province" 
@@ -36,14 +36,14 @@
           </el-select>
         </el-form-item>
         <el-form-item label="题型" prop="questionType">
-          <el-radio-group  v-model="addForm.questionType">
+          <el-radio-group  v-model="addForm.questionType"  @change="changeType" >
             <el-radio :label="1">单选</el-radio>
             <el-radio :label="2">多选</el-radio>
             <el-radio :label="3">简答</el-radio>
           </el-radio-group>
         </el-form-item>
-        <el-form-item label="难度" prop="difficult">
-          <el-radio-group  v-model="addForm.difficult">
+        <el-form-item label="难度" prop="difficulty">
+          <el-radio-group  v-model="addForm.difficulty">
             <el-radio :label="1">简单</el-radio>
             <el-radio :label="2">一般</el-radio>
             <el-radio :label="3">困难</el-radio>
@@ -52,14 +52,28 @@
         <el-form-item label="题干" prop="question">
           <quill-editor v-model="addForm.question" style="height:300px;width:600px"></quill-editor>
         </el-form-item>
-        <el-form-item label="选项" style="marginTop:150px">
+        <el-form-item v-if="addForm.questionType!==3" label="选项" style="marginTop:150px">
           <span>以下选中的选项为正确答案</span>
-          <p>此处是要添加的选项</p>
+        <el-form-item v-for="(item,index) in addForm.options" :key="index">
+          <!-- 单选 -->
+          <el-radio-group @change="changeOption"   v-if="addForm.questionType===1" v-model="selectOption">
+            <el-radio :label="item.code" :value="item.code" >
+              {{item.code}}
+            </el-radio>
+          </el-radio-group>
+          <!-- 多选 -->
+          <template v-if="addForm.questionType === 2">
+            <el-checkbox v-model="item.isRight" :label="item.code" :value="item.code">
+            {{item.code}}
+            </el-checkbox>
+          </template>
+          <el-input style="width:300px;margin:10px 20px;" v-model="item.title"></el-input ><el-button @click="delOptions(index)">删除该选项</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-button type="primary">增加选项与答案</el-button>
         </el-form-item>
-        <el-form-item label="解析视频" prop="videoURL">
+        <el-form-item v-if="addForm.questionType!==3">
+          <el-button type="primary" @click="addQuestion">增加选项与答案</el-button>
+        </el-form-item>
+        <el-form-item label="解析视频" prop="videoURL" :style="{marginTop:addForm.questionType===3?'150px':'0'}">
           <el-input v-model="addForm.videoURL" style="width:400px"></el-input>
         </el-form-item>
         <el-form-item label="答案解析" prop="answer">
@@ -83,43 +97,45 @@
 </template>
 
 <script>
+import {add} from '../../api/hmmm/questions'
 import {simple as subjectList} from '../../api/hmmm/subjects'
 import {simple as catalogList} from '../../api/hmmm/directorys'
 import {simple as tagList} from '../../api/hmmm/tags'
 import {list} from '../../api/hmmm/companys'
 import {provinces,citys} from '../../api/hmmm/citys'
-import {difficulty,questionType,direction} from '../../api/hmmm/constants'
+import {difficultyy,questionType,direction} from '../../api/hmmm/constants'
 export default {
   
   name: 'QuestionsNew',
   data() {
     return {
+      codeStr:'abcdefghijklmnopqrstuvwxyz',
       subject:[],
       catalog:[],
       company:[],
       direction,
-      questionType,
-      difficulty,
       tag:[],
       city:[],
       province:[],
+      selectOption:'',
       addForm:{
         // number:'',//试题编号
-        subjectID:'',//学科
-        catalogID:'',//目录
-        enterpriseID:'',//企业
-        city:'',//城市
-        province:'',//省份
+        subjectID:null,//学科
+        catalogID:null,//目录
+        enterpriseID:null,//企业
+        city:null,//城市
+        province:null,//省份
         direction:'',//方向
-        questionType:'',//题型
-        difficult:'',//难度
+        questionType:1,//题型
+        difficulty:1,//难度
         question:'',//题干
-        options:{},//选项
+        options:[],//选项
         videoURL:'',//解析视频
         answer:'',//答案解析
         remarks:'',//题目备注
-        tags:'',//试题标签
+        tags:null,//试题标签
       },
+      // 表单自动校验规则
       addRules:{
          subjectID:[
            {required:true,message:'请选择学科'}
@@ -142,7 +158,7 @@ export default {
          questionType:[
            {required:true,message:'请选择题型'}
          ],
-         difficult:[
+         difficulty:[
            {required:true,message:'请选择难度'}
          ],
          question:[
@@ -165,6 +181,36 @@ export default {
     }
   },
   methods:{
+    // 删除该选项
+    async delOptions(index){
+      await this.$confirm('确定要删除该选项？')
+      this.addForm.options.splice(index,1)
+    },
+    // 单选时改变选项
+    changeOption(){
+      this.addForm.options = this.addForm.options.map(item =>{
+        item.code === this.selectOption? (item.isRight=true):(item.isRight=false)
+        return item;
+      })
+
+    },  
+    // 改变题目类型 
+    changeType(){
+      this.addForm.options = this.addForm.options.map(item =>{
+        item.isRight = false
+        item.title = ''
+        return item
+      })
+
+    }, 
+    // 增加题目
+    addQuestion(){
+      this.addForm.options.code = this.codeStr.charAt(this.addForm.options.length).toUpperCase()
+      this.addForm.options.push({
+        code:this.addForm.options.code,
+        title:'',
+        isRight:false})
+    },
     // 获取简单学科列表
     getSubject(){
       subjectList().then(res =>{this.subject = res.data})
@@ -193,16 +239,22 @@ export default {
       this.addForm.city = this.city[0]
     },
      
-    // 表单验证
-     submitForm() {
-        this.$refs.addForm.validate((valid) => {
-          if (valid) {
-            alert('录入成功');
-          } else {
-            console.log('提交失败');
-            return false;
-          }
-        });
+    // 提交表单验证
+     async submitForm() {
+       await this.$refs.addForm.validate()
+       this.addForm.direction=this.addForm.direction.toString()
+       this.addForm.tags=this.addForm.tags.toString()
+       this.addForm.difficulty=this.addForm.difficulty.toString()
+       this.addForm.questionType=this.addForm.questionType.toString()
+       await add(this.addForm)
+      //  await this.$refs.addForm.validate((isOK) => {
+      //     if (isOK) {
+      //       alert('录入成功');
+      //     } else {
+      //       console.log('提交失败');
+      //       return false;
+      //     }
+      //   });
       },
       resetForm() {
         this.$refs.addForm.resetFields();
@@ -216,7 +268,7 @@ export default {
     this.getTag(),
     this.getCity(),
     this.getProvince()
-    
+    // this.addQuestion()
   }
 }
 </script>
